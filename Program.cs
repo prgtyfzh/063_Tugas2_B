7c15d5b0b0f2206b4a1c3df324d07108346051e3
prgtyfzh <pragitya.faazha.ft20@mail.umy.ac.id>
2022-03-25 02:49:41 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace EXEPABD
+{
+    class Program
+    {
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=DESKTOP-NH7J05B;database=Exercise;Integrated Security=TRUE");
+                con.Open();
+
+                SqlCommand cm = new SqlCommand("create table Pegawai (ID_Pegawai char(5) not null primary key, Nama_Pegawai varchar(30), Alamat varchar(50), Jabatan varchar(20))" +
+                    ("create table Pelanggan (ID_Pelanggan char(5) not null primary key, Nama_Pelanggan varchar(30), Alamat varchar(50))" +
+                    ("create table Obat (ID_Obat char(5) not null primary key, Nama_Obat varchar(30), Jenis varchar(20), Harga decimal(15,0), Stok varchar(20))"), con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Tabel sukses dibuat!");
+                Console.ReadKey();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Oops, sepertinya ada yang salah." + e);
+                Console.ReadKey();
+            }
+
+            finally
+            {
+                con.Close();
+            }
+        }
+        static void Main(string[] args)
+        {
+            new Program().CreateTable();
+        }
+    }
+}
