b47a598af15e99e7610594ea184fbccdb7c35a6a
prgtyfzh <pragitya.faazha.ft20@mail.umy.ac.id>
2022-03-25 03:40:32 +0700

    Memasukkan Data Pelanggan dan Obat

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace EXEPABD
 
                 SqlCommand cm = new SqlCommand("create table Pegawai (ID_Pegawai char(5) not null primary key, Nama_Pegawai varchar(30), Alamat varchar(50), Jabatan varchar(20))" +
                     ("create table Pelanggan (ID_Pelanggan char(5) not null primary key, Nama_Pelanggan varchar(30), Alamat varchar(50))" +
-                    ("create table Obat (ID_Obat char(5) not null primary key, Nama_Obat varchar(30), Jenis varchar(20), Harga decimal(15,0), Stok varchar(20))"), con);
+                    ("create table Obat (ID_Obat char(5) not null primary key, Nama_Obat varchar(30), Jenis varchar(20), Harga char(10), Stok varchar(20))")), con);
                 cm.ExecuteNonQuery();
 
                 Console.WriteLine("Tabel sukses dibuat!");
@@ -46,7 +46,21 @@ namespace EXEPABD
                 con = new SqlConnection("data source=DESKTOP-NH7J05B;database=Exercise;Integrated Security=TRUE");
                 con.Open();
 
-                SqlCommand cm = new SqlCommand("insert into Pegawai (ID_Pegawai, Nama_Pegawai, Alamat, Jabatan) values ('KP001', 'Asri', 'Bandung', 'Apoteker')", con);
+                SqlCommand cm = new SqlCommand("insert into Pegawai (ID_Pegawai, Nama_Pegawai, Alamat, Jabatan) values ('KP001', 'Asri', 'Bandung', 'Apoteker')" +
+                    ("insert into Pegawai (ID_Pegawai, Nama_Pegawai, Alamat, Jabatan) values ('KP002', 'Farah', 'Jakarta', 'Asisten Apoteker')") +
+                    ("insert into Pegawai (ID_Pegawai, Nama_Pegawai, Alamat, Jabatan) values ('KP003', 'Daniah', 'Surabaya', 'Seksi Penjualan')") +
+                    ("insert into Pegawai (ID_Pegawai, Nama_Pegawai, Alamat, Jabatan) values ('KP004', 'Farhan', 'Bandung', 'Seksi Gudang')") +
+                    ("insert into Pegawai (ID_Pegawai, Nama_Pegawai, Alamat, Jabatan) values ('KP005', 'Marco', 'Jakarta', 'Seksi Pembelian')") +
+                    ("insert into Pelanggan (ID_Pelanggan, Nama_Pelanggan, Alamat) values ('IP001', 'Cava', 'Bandung')") +
+                    ("insert into Pelanggan (ID_Pelanggan, Nama_Pelanggan, Alamat) values ('IP002', 'Awaf', 'Bandung')") +
+                    ("insert into Pelanggan (ID_Pelanggan, Nama_Pelanggan, Alamat) values ('IP003', 'Rei', 'Bandung')") +
+                    ("insert into Pelanggan (ID_Pelanggan, Nama_Pelanggan, Alamat) values ('IP004', 'Faza', 'Bandung')") +
+                    ("insert into Pelanggan (ID_Pelanggan, Nama_Pelanggan, Alamat) values ('IP005', 'Kaneish', 'Bandung')") +
+                    ("insert into Obat (ID_Obat, Nama_Obat, Jenis, Harga, Stok) values ('KO001', 'Amoxicillin', 'Tablet', '5000', '100')") +
+                    ("insert into Obat (ID_Obat, Nama_Obat, Jenis, Harga, Stok) values ('KO002', 'Paracetamol', 'Tablet', '20000', '100')") +
+                    ("insert into Obat (ID_Obat, Nama_Obat, Jenis, Harga, Stok) values ('KO003', 'Fenofibrate', 'Tablet', '150000', '100')") +
+                    ("insert into Obat (ID_Obat, Nama_Obat, Jenis, Harga, Stok) values ('KO004', 'Lincomycin', 'Kapsul', '118000', '100')") +
+                    ("insert into Obat (ID_Obat, Nama_Obat, Jenis, Harga, Stok) values ('KO005', 'Domperidone', 'Sirup', '18000', '100')"), con);
                 cm.ExecuteNonQuery();
 
                 Console.WriteLine("Data berhasil ditambahkan");
@@ -67,6 +81,7 @@ namespace EXEPABD
         static void Main(string[] args)
         {
             new Program().CreateTable();
+            new Program().InsertData();
         }
     }
 }
